f09bb9fad7244f42351560240c7d837b80af26e7
Sandorex <rzhw3h@gmail.com>
2022-01-08 04:43:21 +0100

    Block: making space the median line only between letters

--- block/configure.py
+++ block/configure.py
@@ -17,7 +17,7 @@
 #
 # configure.py: Configures font project for fontforge
 
-import fontforge, os, string
+import fontforge, os, string, sys, glob, psMat
 
 from generate import OUTPUT_DIR, GLYPH_OUTPUT_DIR
 
@@ -29,30 +29,46 @@ def configure():
 
     print("Configuring font '{} {}'".format(font.familyname, font.version))
 
-    def new_char(ch, outline=None, width=CHAR_WIDTH):
-        char = font.createChar(ord(ch))
+    def glyph_path(index):
+        return os.path.join(OUTPUT_DIR, GLYPH_OUTPUT_DIR, str(index) + '.svg')
+
+    def new_char(ch=None, outline=None, *, name=None, width=CHAR_WIDTH):
+        if name is not None:
+            char = font.createChar(-1, name)
+        else:
+            char = font.createChar(ord(ch))
+
         char.width = width
 
-        path = os.path.join(OUTPUT_DIR, GLYPH_OUTPUT_DIR, str(outline) + '.svg')
-        if outline is not None and os.path.exists(path):
-            char.importOutlines(path)
+        if outline is not None:
+            if os.path.exists(outline):
+                char.importOutlines(outline)
+            else:
+                print('Error: glyph outline "{}" not found'.format(outline))
+                sys.exit(1)
 
         char.removeOverlap() # important
 
         return char
 
+    # add all the glyphs
+    # for file in glob.glob(os.path.join(OUTPUT_DIR, GLYPH_OUTPUT_DIR, '*.svg')):
+    #     filename = os.path.splitext(os.path.basename(file))[0]
+    #     new_char(name='blockx' + filename, outline=file)
+
     # TODO: optionally read from external file the values for characters
     # NOTE: for testing only! proper layout will be done at later date
     for i, c in enumerate(string.ascii_uppercase, 2):
-        new_char(c, i)
+        new_char(c, glyph_path(i))
 
     for i, c in enumerate(string.ascii_lowercase, 2 + len(string.ascii_uppercase)):
-        new_char(c, i)
+        new_char(c, glyph_path(i))
 
     # special characters
 
     # space is going to be double width for visibility
-    new_char(' ', 0, width=CHAR_WIDTH*2)
+    new_char(' ', width=CHAR_WIDTH*2)
+    new_char(name='blockspace', width=CHAR_WIDTH*2)
 
     # this is 2 times the width space
     new_char(',', width=CHAR_WIDTH * 2)
