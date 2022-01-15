dc97af2744974ec61c779c917077682a2b235fde
Sandorex <rzhw3h@gmail.com>
2022-01-15 08:08:42 +0100

    Made a layout for the letters, upper and lower are the same Updated font for preview and fixed the bolding annoyance

--- block/config.py
+++ block/config.py
@@ -20,13 +20,90 @@
 import configure as c
 
 def generate():
+    # TODO: sort out the build scripts so a proper user.config.py can be made
     bindings = {
-        "a": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U2],
-        "b": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_D1],
-        "c": [c.BLOCK_LINE, c.BLOCK_D1],
-        "e": [c.BLOCK_LINE, c.BLOCK_U1]
-        # TODO: the rest
+        # '  .|   '
+        "a": [c.BLOCK_LINE, c.BLOCK_U3],
+
+        # '  .|.  '
+        "b": [c.BLOCK_LINE, c.BLOCK_U3, c.BLOCK_D1],
+
+        # '   |.. '
+        "c": [c.BLOCK_LINE, c.BLOCK_D1, c.BLOCK_D2],
+
+        # ' ..|   '
+        "d": [c.BLOCK_LINE, c.BLOCK_U2, c.BLOCK_U3],
+
+        # ' . |   '
+        "e": [c.BLOCK_LINE, c.BLOCK_U2],
+
+        # '...|   '
+        "f": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U2, c.BLOCK_U3],
+
+        # '   |.  '
+        "g": [c.BLOCK_LINE, c.BLOCK_D1],
+
+        # ' ..|.  '
+        "h": [c.BLOCK_LINE, c.BLOCK_U2, c.BLOCK_U3, c.BLOCK_D1],
+
+        # '. .|   '
+        "i": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U3],
+
+        # '. .|.. '
+        "j": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U3, c.BLOCK_D1, c.BLOCK_D2],
+
+        # ' . | . '
+        "k": [c.BLOCK_LINE, c.BLOCK_U2, c.BLOCK_D2],
+
+        # ' ..|.. '
+        "l": [c.BLOCK_LINE, c.BLOCK_U2, c.BLOCK_U3, c.BLOCK_D1, c.BLOCK_D2],
+
+        # '  .|...'
+        "m": [c.BLOCK_LINE, c.BLOCK_U3, c.BLOCK_D1, c.BLOCK_D2, c.BLOCK_D3],
+
+        # '  .|. .'
+        "n": [c.BLOCK_LINE, c.BLOCK_U3, c.BLOCK_D1, c.BLOCK_D3],
+
+        # '.  |   '
+        "o": [c.BLOCK_LINE, c.BLOCK_U1],
+
+        # '.. |   '
+        "p": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U2],
+
+        # ' ..|...'
+        "q": [c.BLOCK_LINE, c.BLOCK_U2, c.BLOCK_U3, c.BLOCK_D1, c.BLOCK_D2, c.BLOCK_D3],
+
+        # '   | . '
+        "r": [c.BLOCK_LINE, c.BLOCK_D2],
+
+        # '   | ..'
+        "s": [c.BLOCK_LINE, c.BLOCK_D2, c.BLOCK_D3],
+
+        # '   |...'
+        "t": [c.BLOCK_LINE, c.BLOCK_D1, c.BLOCK_D2, c.BLOCK_D3],
+
+        # '.. | ..'
+        "u": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U2, c.BLOCK_D2, c.BLOCK_D3],
+
+        # '.. |.. '
+        "v": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U2, c.BLOCK_D1, c.BLOCK_D2],
+
+        # '.. |...'
+        "w": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U2, c.BLOCK_D1, c.BLOCK_D2, c.BLOCK_D3],
+
+        # '...|.. '
+        "x": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U2, c.BLOCK_U3, c.BLOCK_D1, c.BLOCK_D2],
+
+        # ' ..|...'
+        "y": [c.BLOCK_LINE, c.BLOCK_U2, c.BLOCK_U3, c.BLOCK_D1, c.BLOCK_D2, c.BLOCK_D3],
+
+        # '. .|...'
+        "z": [c.BLOCK_LINE, c.BLOCK_U1, c.BLOCK_U3, c.BLOCK_D1, c.BLOCK_D2, c.BLOCK_D3],
     }
+
+    # copy bindings for uppercase letters
+    for k, v in dict(bindings).items():
+        bindings[k.upper()] = v
 
     special = {}
 
@@ -53,3 +130,6 @@ def generate():
             bindings[str(i)] = blocks
 
     return bindings, special
+
+if __name__ == '__main__':
+    print('Do not run the config, run the configure.py script')
